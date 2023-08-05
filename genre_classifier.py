0d7b7172a85f0dee3e06f856fb5059131b363256
Edward Lee <7edwardlee@gmail.com>
2023-08-05 00:30:22 -0700

    more optimization of model

--- genre_classifier.py
+++ genre_classifier.py
@@ -1,32 +1,53 @@
 import json
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
-import matplotlib.pyplot as plt
+from tensorflow.keras.layers import (
+    Conv2D, MaxPooling2D, BatchNormalization,
+    Flatten, Dense, Dropout, LeakyReLU
+)
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.preprocessing import StandardScaler
 
-DATASET_PATH = "data.json"
+
+DATA_PATH = "kpopdata.json"
 
 
-def load_data(dataset_path):
-    with open(dataset_path, "r") as fp:
+def load_data(data_path):
+    """Loads training dataset from json file.
+
+        :param data_path (str): Path to json file containing data
+        :return X (ndarray): Inputs
+        :return y (ndarray): Targets
+    """
+
+    with open(data_path, "r") as fp:
         data = json.load(fp)
 
-    # convert lists into numpy arrays
-    inputs = np.array(data["mfcc"])
-    targets = np.array(data["labels"])
-
-    return inputs, targets
+    X = np.array(data["mfcc"])
+    y = np.array(data["labels"])
+    return X, y
 
 
 def plot_history(history):
+    """Plots accuracy/loss for training/validation set as a function of the epochs
+
+        :param history: Training history of model
+        :return:
+    """
+
     fig, axs = plt.subplots(2)
 
+    # create accuracy sublpot
     axs[0].plot(history.history["accuracy"], label="train accuracy")
     axs[0].plot(history.history["val_accuracy"], label="test accuracy")
     axs[0].set_ylabel("Accuracy")
     axs[0].legend(loc="lower right")
     axs[0].set_title("Accuracy eval")
 
+    # create error sublpot
     axs[1].plot(history.history["loss"], label="train error")
     axs[1].plot(history.history["val_loss"], label="test error")
     axs[1].set_ylabel("Error")
@@ -37,43 +58,123 @@ def plot_history(history):
     plt.show()
 
 
+def prepare_datasets(test_size, validation_size):
+    X, y = load_data(DATA_PATH)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_size, random_state=42)
+
+    # Reshape the MFCC data for the StandardScaler
+    num_time_steps, num_mfcc = X_train.shape[1], X_train.shape[2]
+    X_train = X_train.reshape(-1, num_time_steps * num_mfcc)
+    X_validation = X_validation.reshape(-1, num_time_steps * num_mfcc)
+    X_test = X_test.reshape(-1, num_time_steps * num_mfcc)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_validation = scaler.transform(X_validation)
+    X_test = scaler.transform(X_test)
+
+    X_train = X_train.reshape(-1, num_time_steps, num_mfcc, 1)
+    X_validation = X_validation.reshape(-1, num_time_steps, num_mfcc, 1)
+    X_test = X_test.reshape(-1, num_time_steps, num_mfcc, 1)
+
+    return X_train, X_validation, X_test, y_train, y_validation, y_test
+
+
+def build_model(input_shape, num_classes):
+    model = keras.Sequential()
+
+    model.add(Conv2D(64, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+
+    model.add(Conv2D(64, (2, 2), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(Dropout(0.3))
+
+    model.add(Dense(num_classes, activation='softmax'))
+
+    return model
+
+
+def train_model(X_train, y_train, X_validation, y_validation, input_shape, num_classes, epochs=30):
+    model = build_model(input_shape, num_classes)
+    model.compile(optimizer=Adam(learning_rate=0.0001),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    datagen = ImageDataGenerator(
+        width_shift_range=0.2, height_shift_range=0.2,
+        rotation_range=15, zoom_range=0.2
+    )
+    datagen.fit(X_train)
+
+    history = model.fit(
+        datagen.flow(X_train, y_train, batch_size=32),
+        steps_per_epoch=len(X_train) / 32,
+        epochs=epochs,
+        validation_data=(X_validation, y_validation)
+    )
+
+    # Train the model without augmentation for a few more epochs
+    history = model.fit(
+        X_train, y_train,
+        validation_data=(X_validation, y_validation),
+        batch_size=32,
+        epochs=epochs
+    )
+
+    return model, history
+
+
+def predict(model, X, y):
+    """Predict a single sample using the trained model
+
+    :param model: Trained classifier
+    :param X: Input data
+    :param y (int): Target
+    """
+
+    # add a dimension to input data for sample - model.predict() expects a 4d array in this case
+    X = X[np.newaxis, ...]  # array shape (1, 130, 13, 1)
+
+    # perform prediction
+    prediction = model.predict(X)
+
+    # get index with max value
+    predicted_index = np.argmax(prediction, axis=1)
+
+    print("Target: {}, Predicted label: {}".format(y, predicted_index))
+
+
 if __name__ == "__main__":
-    # load dataset
-    inputs, targets = load_data(DATASET_PATH)
+    X_train, X_validation, X_test, y_train, y_validation, y_test = prepare_datasets(0.25, 0.2)
 
-    # split data into training and test sets
-    inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs,
-                                                                              targets,
-                                                                              test_size=0.3)
-    # build network architecture
-    model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
+    input_shape = (X_train.shape[1], X_train.shape[2], 1)
+    num_classes = len(np.unique(y_train))
+    model, history = train_model(X_train, y_train, X_validation, y_validation, input_shape, num_classes, epochs=50)
 
-        keras.layers.Dense(512, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
-        keras.layers.Dropout(0.3),
-
-        keras.layers.Dense(256, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
-        keras.layers.Dropout(0.3),
-
-        keras.layers.Dense(64, activation="relu", kernel_regularizer=keras.regularizers.l2(0.001)),
-        keras.layers.Dropout(0.3),
-
-        keras.layers.Dense(10, activation="softmax")
-    ])
-
-    # compile network
-
-    optimizer = keras.optimizers.Adam(learning_rate=0.0001)
-    model.compile(optimizer=optimizer,
-                  loss="sparse_categorical_crossentropy",
-                  metrics=["accuracy"])
-
-    model.summary()
-
-    # train network
-    history = model.fit(inputs_train, targets_train,
-                        validation_data=(inputs_test, targets_test),
-                        epochs=100,
-                        batch_size=32)
+    model.save('saved_model.h5')
 
     plot_history(history)
+
+    test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+    print('\nTest accuracy:', test_acc)
+
+    # Pick a sample to predict from the test set
+    sample_idx = 100
+    X_to_predict = X_test[sample_idx]
+    y_to_predict = y_test[sample_idx]
+
+    # Predict sample
+    predict(model, X_to_predict, y_to_predict)
