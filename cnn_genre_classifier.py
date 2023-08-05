5395c441138130ef898db398a633156da70c5469
Johnslee123 <116469694+Johnslee123@users.noreply.github.com>
2023-08-04 19:02:34 -0700

    test.py works with random classical .wav file from internet

--- cnn_genre_classifier.py
+++ cnn_genre_classifier.py
@@ -1,79 +1,51 @@
 import json
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import tensorflow.keras as keras
-import matplotlib.pyplot as plt
+from tensorflow.keras.layers import (
+    Conv2D, MaxPooling2D, BatchNormalization,
+    Flatten, Dense, Dropout, LeakyReLU
+)
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-DATASET_PATH = "data.json"
+DATA_PATH = "data.json"
 
 
-def load_data(dataset_path):
-    with open(dataset_path, "r") as fp:
+def load_data(data_path):
+    """Loads training dataset from json file.
+
+        :param data_path (str): Path to json file containing data
+        :return X (ndarray): Inputs
+        :return y (ndarray): Targets
+    """
+
+    with open(data_path, "r") as fp:
         data = json.load(fp)
 
-    # convert lists into numpy arrays
     X = np.array(data["mfcc"])
     y = np.array(data["labels"])
-
     return X, y
 
 
-def prepare_datasets(test_size, validation_size):
-    X, y = load_data(DATASET_PATH)
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
-
-    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_size)
-
-    X_train = X_train[..., np.newaxis]
-    X_validation = X_validation[..., np.newaxis]
-    X_test = X_test[..., np.newaxis]
-
-    return X_train, X_validation, X_test, y_train, y_validation, y_test
-
-
-def build_model(input_shape):
-    model = keras.Sequential()
-
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same'))
-    model.add(keras.layers.BatchNormalization())
-
-    model.add(keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPool2D((3, 3), strides=(2, 2), padding='same'))
-    model.add(keras.layers.BatchNormalization())
-
-    model.add(keras.layers.Conv2D(32, (2, 2), activation='relu', input_shape=input_shape))
-    model.add(keras.layers.MaxPool2D((2, 2), strides=(2, 2), padding='same'))
-    model.add(keras.layers.BatchNormalization())
-
-    model.add(keras.layers.Flatten())
-    model.add(keras.layers.Dense(64, activation='relu'))
-    model.add(keras.layers.Dropout(0.3))
-
-    model.add(keras.layers.Dense(10, activation='softmax'))
-
-    return model
-
-
-def predict(model, X, y):
-    X = X[np.newaxis, ...]
-
-    prediction = model.predict(X)
-
-    predicted_index = np.argmax(prediction, axis=1)
-    print("Expected index: {}, Predicted index: {}".format(y, predicted_index))
-
-
 def plot_history(history):
+    """Plots accuracy/loss for training/validation set as a function of the epochs
+
+        :param history: Training history of model
+        :return:
+    """
+
     fig, axs = plt.subplots(2)
 
+    # create accuracy sublpot
     axs[0].plot(history.history["accuracy"], label="train accuracy")
     axs[0].plot(history.history["val_accuracy"], label="test accuracy")
     axs[0].set_ylabel("Accuracy")
     axs[0].legend(loc="lower right")
     axs[0].set_title("Accuracy eval")
 
+    # create error sublpot
     axs[1].plot(history.history["loss"], label="train error")
     axs[1].plot(history.history["val_loss"], label="test error")
     axs[1].set_ylabel("Error")
@@ -84,33 +56,133 @@ def plot_history(history):
     plt.show()
 
 
-if __name__ == "__main__":
-    X_train, X_validation, X_test, y_train, y_validation, y_test = prepare_datasets(0.25, 0.2)
+def prepare_datasets(test_size, validation_size):
+    """Loads data and splits it into train, validation and test sets.
 
-    input_shape = (X_train.shape[1], X_train.shape[2])
-    model = build_model(input_shape)
+    :param test_size (float): Value in [0, 1] indicating percentage of data set to allocate to test split
+    :param validation_size (float): Value in [0, 1] indicating percentage of train set to allocate to validation split
 
-    optimizer = keras.optimizers.Adam(learning_rate=0.0001)
-    model.compile(optimizer=optimizer,
-                  loss="sparse_categorical_crossentropy",
+    :return X_train (ndarray): Input training set
+    :return X_validation (ndarray): Input validation set
+    :return X_test (ndarray): Input test set
+    :return y_train (ndarray): Target training set
+    :return y_validation (ndarray): Target validation set
+    :return y_test (ndarray): Target test set
+    """
+
+    # load data
+    X, y = load_data(DATA_PATH)
+
+    # create train, validation and test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    X_train, X_validation, y_train, y_validation = train_test_split(X_train, y_train, test_size=validation_size)
+
+    # add an axis to input sets
+    X_train = X_train[..., np.newaxis]
+    X_validation = X_validation[..., np.newaxis]
+    X_test = X_test[..., np.newaxis]
+
+    return X_train, X_validation, X_test, y_train, y_validation, y_test
+
+
+def build_model(input_shape, num_classes):
+    model = keras.Sequential()
+
+    model.add(Conv2D(64, (3, 3), activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((3, 3), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+
+    model.add(Conv2D(64, (2, 2), activation='relu'))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2), padding='same'))
+    model.add(BatchNormalization())
+
+    model.add(Flatten())
+    model.add(Dense(128))
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(Dropout(0.3))
+
+    model.add(Dense(num_classes, activation='softmax'))
+
+    return model
+
+
+def train_model(X_train, y_train, X_validation, y_validation, input_shape, num_classes, epochs=30):
+    model = build_model(input_shape, num_classes)
+    model.compile(optimizer=Adam(learning_rate=0.0001),
+                  loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
 
-    model.summary()
+    datagen = ImageDataGenerator(
+        width_shift_range=0.2, height_shift_range=0.2,
+        rotation_range=15, zoom_range=0.2
+    )
+    datagen.fit(X_train)
 
-    history = model.fit(X_train, y_train,
-                        validation_data=(X_validation, y_validation),
-                        batch_size=32,
-                        epochs=30)
+    history = model.fit(
+        datagen.flow(X_train, y_train, batch_size=32),
+        steps_per_epoch=len(X_train) / 32,
+        epochs=epochs,
+        validation_data=(X_validation, y_validation)
+    )
 
+    # Train the model without augmentation for a few more epochs
+    history = model.fit(
+        X_train, y_train,
+        validation_data=(X_validation, y_validation),
+        batch_size=32,
+        epochs=epochs
+    )
+
+    return model, history
+
+
+def predict(model, X, y):
+    """Predict a single sample using the trained model
+
+    :param model: Trained classifier
+    :param X: Input data
+    :param y (int): Target
+    """
+
+    # add a dimension to input data for sample - model.predict() expects a 4d array in this case
+    X = X[np.newaxis, ...]  # array shape (1, 130, 13, 1)
+
+    # perform prediction
+    prediction = model.predict(X)
+
+    # get index with max value
+    predicted_index = np.argmax(prediction, axis=1)
+
+    print("Target: {}, Predicted label: {}".format(y, predicted_index))
+
+
+if __name__ == "__main__":
+    # Load and prepare data
+    X_train, X_validation, X_test, y_train, y_validation, y_test = prepare_datasets(0.25, 0.2)
+
+    # Build and train model
+    input_shape = (X_train.shape[1], X_train.shape[2], 1)
+    num_classes = len(np.unique(y_train))
+    model, history = train_model(X_train, y_train, X_validation, y_validation, input_shape, num_classes, epochs=50)
+
+    # Save the trained model
+    model.save('saved_model.h5')
+
+    # Plot accuracy/error for training and validation
     plot_history(history)
 
+    # Evaluate model on test set
     test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
     print('\nTest accuracy:', test_acc)
 
-    # test_error, test_accuracy = model.evaluate(X_test, y_test, verbose=1)
-    # print("Accuracy on test set is: {}".format(test_accuracy))
-    #
-    #
-    # X = X_test[100]
-    # y = y_test[100]
-    # predict(model, X, y)
+    # Pick a sample to predict from the test set
+    sample_idx = 100
+    X_to_predict = X_test[sample_idx]
+    y_to_predict = y_test[sample_idx]
+
+    # Predict sample
+    predict(model, X_to_predict, y_to_predict)
---
+++ test.py
@@ -0,0 +1,39 @@
+import librosa
+import numpy as np
+import tensorflow.keras as keras
+
+# Load the trained model
+model = keras.models.load_model('/Users/johnslee/PycharmProjects/MusicAnalysis/saved_model.h5')
+
+
+# Function to extract MFCC features from audio file
+def extract_mfcc(y, sr, num_mfcc=13, n_fft=2048, hop_length=512):
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
+    return mfccs
+
+
+# Load and preprocess the audio data
+audio, sr = librosa.load('classicaltest.wav')
+mfccs = extract_mfcc(y=audio, sr=sr, num_mfcc=13, n_fft=2048, hop_length=512)
+
+# Transpose the MFCCs array to match the shape expected by the model
+mfccs = mfccs.T  # Transpose the array
+
+# Pad or truncate the time steps to match the expected input shape (130 time steps)
+expected_time_steps = 130
+if mfccs.shape[0] < expected_time_steps:
+    mfccs = np.pad(mfccs, ((0, expected_time_steps - mfccs.shape[0]), (0, 0)), mode='constant')
+else:
+    mfccs = mfccs[:expected_time_steps, :]
+
+# Reshape the MFCCs to have the shape (1, time_steps, num_mfcc, 1)
+mfccs = mfccs.reshape(1, mfccs.shape[0], mfccs.shape[1], 1)
+
+# Make a prediction
+predicted_probs = model.predict(mfccs)
+predicted_class = np.argmax(predicted_probs)
+
+class_names = ['pop', 'metal', 'disco', 'blues', 'reggae', 'classical', 'rock', 'hiphop', 'country', 'jazz']
+
+print(f"Predicted class: {class_names[predicted_class]}")
+print(f"Predicted probabilities: {predicted_probs[0]}")
